900e765a747a98b6033bf4f457597ba57d244646
litelx <8joli9@gmail.com>
2021-12-30 21:16:36 +0200

    Task no 1

--- authorization-server.js
+++ authorization-server.js
@@ -3,39 +3,39 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
 const {
-	randomString,
-	containsAll,
-	decodeAuthCredentials,
-	timeout,
+    randomString,
+    containsAll,
+    decodeAuthCredentials,
+    timeout,
 } = require("./utils")
 
 const config = {
-	port: 9001,
-	privateKey: fs.readFileSync("assets/private_key.pem"),
+    port: 9001,
+    privateKey: fs.readFileSync("assets/private_key.pem"),
 
-	clientId: "my-client",
-	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-	redirectUri: "http://localhost:9000/callback",
+    clientId: "my-client",
+    clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+    redirectUri: "http://localhost:9000/callback",
 
-	authorizationEndpoint: "http://localhost:9001/authorize",
+    authorizationEndpoint: "http://localhost:9001/authorize",
 }
 
 const clients = {
-	"my-client": {
-		name: "Sample Client",
-		clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
-		scopes: ["permission:name", "permission:date_of_birth"],
-	},
-	"test-client": {
-		name: "Test Client",
-		clientSecret: "TestSecret",
-		scopes: ["permission:name"],
-	},
+    "my-client": {
+        name: "Sample Client",
+        clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+        scopes: ["permission:name", "permission:date_of_birth"],
+    },
+    "test-client": {
+        name: "Test Client",
+        clientSecret: "TestSecret",
+        scopes: ["permission:name"],
+    },
 }
 
 const users = {
-	user1: "password1",
-	john: "appleseed",
+    user1: "password1",
+    john: "appleseed",
 }
 
 const requests = {}
@@ -54,9 +54,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 Your code here
 */
 
+app.get('/authorize', (req, res) => {
+    res.status(200).end();
+});
+
 const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 })
 
 // for testing purposes
